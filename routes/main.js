ba31e126a3685d43598fc0da13a235dcfd18185f
alextetero <alextetero@gmail.com>
2023-01-17 23:17:34 +0200

    Minimal node server created

---
+++ routes/main.js
@@ -0,0 +1,36 @@
+// routing main /
+const fs = require('fs');
+
+module.exports = function (app) {
+  const isFolder = (path) => {
+    return fs.lstatSync(path).isDirectory() && fs.existsSync(path);
+  };
+
+  app.get('/', (req, res) => {
+    const base = './files/';
+    let path = '';
+
+    if ('path' in req.query) {
+      path = req.query.path;
+    }
+
+    if (isFolder(base + path)) {
+      // if path a folder
+      let files = fs.readdirSync(base + path).map((item) => {
+        const isDir = fs.lstatSync(base + path + '/' + item).isDirectory();
+        let size = 0;
+        if (!isDir) {
+          size = fs.statSync(base + path + '/' + item);
+          console.log(size.size);
+        }
+
+        return {
+          name: item,
+          dir: isDir,
+          size: size.size ?? 0,
+        };
+      });
+      res.json({ path: path, result: true, files: files });
+    }
+  });
+};
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+require('./routes')(app);
+
 app.listen(port, () => {
   console.log(`server running on port: ${port}`);
 });
